ad818aa285b2e11fc23d75e37989eef104e15a25
Brook Heisler <redattack34@gmail.com>
2014-05-31 10:56:38 -0600

    Fixes #1 : Add rendering for headers.

--- src/main/scala/com/castlebravostudios/theguide/gui/TheGuideGui.scala
+++ src/main/scala/com/castlebravostudios/theguide/gui/TheGuideGui.scala
@@ -9,6 +9,7 @@ import com.castlebravostudios.theguide.text.WordWrapper
 import com.castlebravostudios.theguide.text.DefaultTextSizeCalculator
 import com.castlebravostudios.theguide.text.TextLine
 import com.castlebravostudios.theguide.text.RenderableDocument
+import com.castlebravostudios.theguide.markdown.Header
 
 class TheGuideGui extends GuiScreen {
 
@@ -24,7 +25,7 @@ class TheGuideGui extends GuiScreen {
   private[this] def textXSize = 190
   private[this] def textYSize = 280
 
-  private[this] val text = Paragraph( Seq( TextSpan( """Lorem ipsum dolor sit amet,
+  private[this] val text = Seq( Paragraph( Seq( TextSpan( """Lorem ipsum dolor sit amet,
  consectetur adipiscing elit. Vivamus vitae bibendum nulla, in consectetur
  lorem. Phasellus eget libero vitae lorem lacinia mollis ut id nibh. Suspendisse
  at purus mauris. Pellentesque sed risus in nisl consectetur iaculis. Donec
@@ -33,7 +34,8 @@ class TheGuideGui extends GuiScreen {
  Curabitur eget ullamcorper est. Curabitur tempus neque vitae est iaculis
  ultricies. Pellentesque placerat vel justo sit amet sodales. Mauris quis
  elementum lacus, sit amet tempor mi. Nullam et aliquet enim, vel fermentum
- metus. """.filter( c => c != '\n' && c != '\r' ) ) ) )
+ metus. """.filter( c => c != '\n' && c != '\r' ) ) ) ),
+   Header( 1, TextSpan( "Test Header 1" ) ), Header( 2, TextSpan( "Test Header 2" ) ) )
 
   private[this] var document : RenderableDocument = _
 
@@ -50,7 +52,7 @@ class TheGuideGui extends GuiScreen {
     GL11.glScaled(0.5d, 0.5d, 0.5d)
 
     if ( document == null ) {
-      document = RenderableDocument( Seq( text ), textXSize,
+      document = RenderableDocument( text, textXSize,
           new DefaultTextSizeCalculator( fontRenderer ) )
     }
     document.render( (width - textXSize/2) - 5, (height - textYSize/2),
--- src/main/scala/com/castlebravostudios/theguide/text/RenderableElement.scala
+++ src/main/scala/com/castlebravostudios/theguide/text/RenderableElement.scala
@@ -2,28 +2,35 @@ package com.castlebravostudios.theguide.text
 
 import net.minecraft.util.ResourceLocation
 import net.minecraft.client.gui.FontRenderer
+import org.lwjgl.opengl.GL11
 
 case class Link( target : ResourceLocation, startX : Int, endX : Int )
 
 trait RenderableElement {
-  def height( renderer: FontRenderer ) : Int
+  def height( renderer: FontRenderer ) : Int = renderer.FONT_HEIGHT + 1
   def render( x : Int, y : Int, renderer : FontRenderer ) : Unit
 
   val color = 0x404040
 }
 
 case class RenderableHeader( text: String, level : Int ) extends RenderableElement {
-  def height( renderer: FontRenderer ) : Int = 0
-  def render( x: Int, y : Int, renderer: FontRenderer ) : Unit = ()
+  val formattedText = level match {
+    case 1 => "§l§n" + text + "§r"
+    case _ => "§n" + text + "§r"
+  }
+
+  def render( x: Int, y : Int, renderer: FontRenderer ) : Unit = {
+    val textWidth = renderer.getStringWidth(formattedText)
+    val horizOffset = ( 190 - textWidth ) / 2
+    renderer.drawString(formattedText, x + horizOffset, y, color)
+  }
 }
 
 case class TextLine( text : String, links: Set[Link] ) extends RenderableElement {
-  def height( renderer: FontRenderer ) : Int = renderer.FONT_HEIGHT + 1
   def render(x : Int, y : Int, renderer : FontRenderer ) : Unit =
     renderer.drawString(text, x, y, color)
 }
 
 case object BlankLine extends RenderableElement {
-  def height( renderer: FontRenderer ) : Int = renderer.FONT_HEIGHT + 1
   def render(x : Int, y : Int, renderer : FontRenderer ) : Unit = ()
 }
